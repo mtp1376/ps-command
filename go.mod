c86d8ec52ce6b4681f24a612d49f244753f6e56c
Mohammad Teimori Pabandi <mtp1376@gmail.com>
2023-10-12 21:26:25 +0330

    PID, CMD, PPID

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ps-command
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getPpid(pid string) string {
+	statFileAddress := fmt.Sprintf("/proc/%s/stat", pid)
+	content, _ := os.ReadFile(statFileAddress)
+	ppid := strings.Fields(string(content))[3]
+	if ppid != "0" {
+		return ppid
+	}
+	return ""
+}
+
+func main() {
+	entries, _ := os.ReadDir("/proc/")
+	fmt.Printf("PID\tCMD\t\t\t\t\t\t\t\tPPID\n") // Header row
+	for _, entry := range entries {
+		if _, err := strconv.Atoi(entry.Name()); err != nil || !entry.IsDir() {
+			continue
+		}
+		pid := entry.Name()
+		// /proc/{pid}/cmdline
+		cmdlineAddress := fmt.Sprintf("/proc/%s/cmdline", pid)
+		content, _ := os.ReadFile(cmdlineAddress)
+		cleanCmdLine := strings.Replace(string(content), "\000", " ", -1)
+		ppid := getPpid(pid)
+		fmt.Printf("%s\t%s\t\t\t\t\t\t\t\t%s\n", pid, cleanCmdLine, ppid)
+	}
+}
